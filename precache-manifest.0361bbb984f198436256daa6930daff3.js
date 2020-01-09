47e0c4efcc05326e752dfe28d99f5116cb6d957d
Prabesh <prabesh7@gmail.com>
2020-01-09 10:55:06 +0545

    Updates

---
+++ precache-manifest.0361bbb984f198436256daa6930daff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf0642499326bc2d904e2657e871335",
+    "url": "/TaskReminder/index.html"
+  },
+  {
+    "revision": "94ebc8bbbc443cc06061",
+    "url": "/TaskReminder/static/css/main.18ea6727.chunk.css"
+  },
+  {
+    "revision": "3fd046ee09e4dd4d223b",
+    "url": "/TaskReminder/static/js/2.5d28cef3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TaskReminder/static/js/2.5d28cef3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94ebc8bbbc443cc06061",
+    "url": "/TaskReminder/static/js/main.5cfbb84a.chunk.js"
+  },
+  {
+    "revision": "7ca6fe5647a652de5b1b",
+    "url": "/TaskReminder/static/js/runtime-main.28911f77.js"
+  }
+]);
